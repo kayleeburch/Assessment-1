70dc5a06556b6f671a8fab979ece4332bb5793d5
kayleeburch <kayleemburch@gmail.com>
2022-06-03 16:54:11 -0400

    python solution

---
+++ optimal_change.py
@@ -0,0 +1,70 @@
+def optimal_change(item_cost, amount_paid):
+    
+    dollars = {'$100': 100, '$50': 50, '$20': 20, '$10': 10, '$5': 5, '$1': 1} #dollars will hold dollar amount as string (key) and int value (value)
+    cents = {'quarter' : .25, 'dime': .10, 'nickle': .05, 'penny': .01} #cents will hold cent amount as string (key) and int value (value)
+
+    optimal_change = amount_paid - item_cost #optimal_change will hold ideal change to give back
+    optimal_dollar = optimal_change // 1 #using floor to just get dollar amount
+    optimal_cents = round(optimal_change - optimal_dollar, 2) #subtracting total change from dollar amount to get change and then using round to the 2nd place to only return .00 from decimal
+    
+    #storing final amount in lists which will be appended from for loops
+    final_dollar_amount = []
+    final_cent_amount = []
+    
+    # loop through dollars dictionary
+    for dollar in dollars:
+        string_dollar = dollar
+        value_dollar = dollars[dollar]
+        while optimal_dollar >= value_dollar: #set while optimal dollar amount is greater or equal to dollar value in dictionary
+            final_dollar_amount.append(string_dollar) #if greater or equal, append to dollar list
+            optimal_dollar = optimal_dollar - value_dollar #subtract dollar value from optimal dollar amount
+    
+    #doing the same things as above for loop but with cents dictionary
+    for cent in cents:
+        string_cents = cent
+        value_cents = cents[cent]
+        while optimal_cents >= value_cents:
+            final_cent_amount.append(string_cents)
+            optimal_cents = round(optimal_cents - value_cents, 2)
+            
+    
+    #concatenating final dollar amount and final cent amount lists
+    full_change = final_dollar_amount + final_cent_amount
+
+    #calling turn_to_sentence function with concatenated list item_cost and amount_paid parametersn which will be the result returned
+    result = turn_to_sentence(full_change, item_cost, amount_paid)
+    
+    return result
+#creating function that will take in list and set conditions based on items in list and return full string    
+def turn_to_sentence(list, cost, paid):
+    full_string = '' #variable which will hold change amount 
+    x = 0 #setting x to 0 for while loop
+    if len(list) == 0: #to catch exact change and return string no change
+        full_string += 'no change'
+    if len(list) == 1: #if only one item in string, appending 0 to fit conditions 
+        list.append(0)
+    while x < len(list): #looping through the list for length of list
+        counting = list.count(list[x]) #check if plural needed, how many instances are in list
+        if counting > 1: #checking if plural
+            if x + counting == len(list) and list[1] != 0:
+                full_string += 'and '
+            if '$' in list[x]:
+                full_string += f'{counting} {list[x]} bills'
+            elif list[x] == 'penny':
+                full_string += f'{counting} pennies'
+            else:
+                full_string += f'{counting} {list[x]}s'
+        elif counting == 1: #checking if singular
+            if '$' in list[x]:
+                full_string += f'{counting} {list[x]} bill'
+            else:
+                full_string += f'{counting} {list[x]}'
+        x = x + counting #incrementing x by number of instances 
+        if len(list) == 2 and list[1] == 0: #break on condition where only one item in list
+            break
+        if x != len(list): #adding comma as long as not last item on list
+            full_string += ', '
+    
+    return f'The optimal change for an item that costs ${cost} with an amount paid of ${paid} is {full_string}.'
+    
+
---
+++ optimal_change_spec.py
@@ -0,0 +1,8 @@
+from optimal_change import optimal_change
+
+print("1:", optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+print("2:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+print("3:", optimal_change(89.55, 100) == "The optimal change for an item that costs $89.55 with an amount paid of $100 is 1 $10 bill, 1 quarter, and 2 dimes.")
+print("4:", optimal_change(2.00, 400) == "The optimal change for an item that costs $2.0 with an amount paid of $400 is 3 $100 bills, 1 $50 bill, 2 $20 bills, 1 $5 bill, and 3 $1 bills.")
+print("5:", optimal_change(60.00, 60.00) == "The optimal change for an item that costs $60.0 with an amount paid of $60.0 is no change.")
+print("6:", optimal_change(59.99, 60.0) == "The optimal change for an item that costs $59.99 with an amount paid of $60.0 is 1 penny.")# Write your tests here!
